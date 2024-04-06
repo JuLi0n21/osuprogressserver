b0c60e66b6a96e9f77491b85bd24b7722f87017c
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2024-04-06 10:14:32 +0200

    return json if not htmx request

--- api/api.go
+++ api/api.go
@@ -11,11 +11,12 @@ import (
 )
 
 type SearchQuery struct {
-	Query  string
-	From   string
-	To     string
-	Limit  int
-	Offset int
+	query  string
+	from   string
+	to     string
+	limit  int
+	offset int
+	userid int
 }
 
 func (s *Server) ScoreSearch(c *fiber.Ctx) error {
@@ -26,20 +27,30 @@ func (s *Server) ScoreSearch(c *fiber.Ctx) error {
 		return err
 	}
 
-	limit := c.QueryInt("limit", 10)
-	offset := c.QueryInt("offset", 0)
+	q.limit = c.QueryInt("limit", 10)
+	q.offset = c.QueryInt("offset", 0)
+
+	if userid, ok := c.Locals("userid").(int); ok {
+		q.userid = userid
+	} else {
+		q.userid = c.QueryInt("userid")
+	}
+
 	var scores []types.Ext_ScoreData
 
-	scores, err := s.store.GetExtScore(q.Query, 14100399, limit, offset)
+	scores, err := s.store.GetExtScore(q.query, 14100399, q.limit, q.offset)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
 	}
 
-	component := cmp.ScoreContainer(scores, limit, offset+len(scores))
+	if h := c.GetReqHeaders()["Hx-Request"]; len(h) > 0 {
+		component := cmp.ScoreContainer(scores, limit, offset+len(scores))
 
-	handler := adaptor.HTTPHandler(templ.Handler(component))
+		handler := adaptor.HTTPHandler(templ.Handler(component))
 
-	return handler(c)
+		return handler(c)
+	}
 
+	return c.JSON(scores)
 }
